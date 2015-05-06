33a593f18ca75f15213a4daa10c80a5eee33659b
Ian P. Cordero <ianpcordero@gmail.com>
2015-05-05 20:28:58 -0700

    Initial Commit

---
+++ CGRE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CGRE
+//
+//  Created by Ian Cordero on 5/5/15.
+//  Copyright (c) 2015 Ian Cordero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
